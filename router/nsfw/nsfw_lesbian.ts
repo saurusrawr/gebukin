8eb1f9607ec48c6f2722569aa14a7b083e40ff28
saurusrawr <kolisedangapa35@gmail.com>
2026-03-11 21:00:55 +0700

    Create nsfw_lesbian.ts

---
+++ router/nsfw/nsfw_lesbian.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express"
+import axios from "axios"
+
+// pic.re langsung return gambar pake tag query, skibidi
+export default async function creampieHandler(req: Request, res: Response) {
+  try {
+    // pic.re langsung balikin gambar, ga perlu parse json dulu
+    const { data, headers } = await axios.get("https://pic.re/image?tags=lesbian", {
+      responseType: "arraybuffer",
+      timeout: 15000,
+      headers: { 'User-Agent': 'Mozilla/5.0' }
+    })
+
+    const mime = headers['content-type'] || 'image/jpeg'
+    res.set('Content-Type', mime)
+    res.send(Buffer.from(data))
+  } catch (err: any) {
+    res.status(500).json({ status: false, message: err.message })
+  }
+}
